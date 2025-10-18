e834a56292b6ecff039e2f6cff70772580122aaa
Fahad Abdul Qayyum <fahad.ocreateo@gmail.com>
2025-10-18 11:50:22 +0500

    Text added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,7 +140,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations - Fahad
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
